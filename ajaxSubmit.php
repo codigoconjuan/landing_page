01d74fc3aa868426135278d39348fbb62ece4aab
juanpablogdl <juanpablo@69-73.cucea.udg.mx>
2015-05-09 08:46:28 -0500

    corrections made in the contact form

--- ajaxSubmit.php
+++ ajaxSubmit.php
@@ -1,8 +1,8 @@
 <?php
 // video PHP
-$nombre = $_POST['nombre'];
+$name = $_POST['name'];
 $email = $_POST['email'];
-$mensaje = $_POST['mensaje'];
+$message = $_POST['message'];
 
 $header = 'From: ' . $email . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -10,18 +10,18 @@ $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
 
-$comentario .= "Este mensaje fue enviado por: " . $nombre . " \r\n";
-$comentario .= "Su e-mail es: " . $email . " \r\n";
-$comentario .= "Su mensaje es: ". $mensaje ." \r\n";
+$comment .= "This message was sent by: " . $name . " \r\n";
+$comment .= "His email is: " . $email . " \r\n";
+$comment .= "The message is: ". $message ." \r\n";
 
-$para = 'blackmaxxgdl18@hotmail.com';
-$asunto = 'Contacto desde pagina web';
+$for = 'blackmaxxgdl18@hotmail.com';
+$subject = 'Contacto desde pagina web';
 
-mail($para, $asunto, utf8_decode($comentario), $header);
+mail($for, $subject, utf8_decode($comment), $header);
 
 // video validar
-$nombre = $_POST['nombre'];
+$name = $_POST['name'];
 
 echo json_encode(array(
-    'Mensaje' => sprintf('Se recibió tu mail %s', $nombre),
+    'message' => sprintf('Your email has been received %s', $name),
 ));
